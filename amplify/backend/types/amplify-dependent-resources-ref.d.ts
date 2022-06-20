9d7ddbf3697c8dc2658a44e1cabc16e39d14394a
Alloy <esteban.gea@alloytx.com>
2022-06-20 21:34:29 +0200

    end of tutorial

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "amplifyapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "s39941951b": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
